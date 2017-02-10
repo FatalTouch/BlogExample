893ac75d71aeee3d789e23dbbc20cc274fc3c104
Mohit Mittal <contact@mohitmittal.me>
2017-02-11 03:53:45 +0530

    Commented the javascript code and linted with JSCS and JSHint

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,23 +1,31 @@
 (function () {
-    var editButton = document.getElementById("edit-button");
+    'use strict';
+
+    // Element references to be used with the edit post functionality
+    var editButton = document.getElementById('edit-button');
     var subjectElement = document.getElementsByName('subject')[0];
     var contentElement = document.getElementsByName('content')[0];
     var subjectElementMain = document.getElementsByClassName('subject')[0];
     var contentElementMain = document.getElementsByClassName('content')[0];
     var postIdElement = document.getElementsByName('post_id')[0];
     var errorElement = document.getElementById('error');
-    var editCommentElements = document.getElementsByClassName('edit-comment');
 
+    // Check if edit button was found and attach the handler
     if (editButton) {
         editButton.onclick = function () {
+
+            // Hide the post that is originally shown
             var post = document.getElementsByClassName('post')[0];
-            post.style.display = "none";
+            post.style.display = 'none';
 
+            // Display our post editor
             var editForm = document.getElementsByClassName('edit-post')[0];
-            editForm.style.display = "block";
+            editForm.style.display = 'block';
 
+            // Copy subject value from unedited post
             subjectElement.value = document.getElementsByClassName('subject')[0].innerHTML;
 
+            // Copy the content value from unedited post and replace html line breaks with newlines
             var content = document.getElementsByClassName('content')[0].innerHTML;
             content = content.replace(/<br>/g, '\n');
             contentElement.value = content;
@@ -25,34 +33,49 @@
         };
     }
 
+    // Reference to cancel edit button
     var cancelEditButton = document.getElementById('cancel-edit');
 
+    // Attach event handler if button exists and cancel the post editing
+    // by showing the un-edited post and hiding the edit form.
     if (cancelEditButton) {
         cancelEditButton.onclick = function () {
             var post = document.getElementsByClassName('post')[0];
-            post.style.display = "block";
+            post.style.display = 'block';
 
             var editForm = document.getElementsByClassName('edit-post')[0];
-            editForm.style.display = "none";
-        }
+            editForm.style.display = 'none';
+        };
     }
 
+    // Get our edit form and attach onsubmit event handler if it exists
     var form = document.getElementById('edit-form');
     if (form) {
         form.onsubmit = function () {
-            var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+            // Create new ajax request with fallback to ActiveXobject
+            var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
+
+            // Attach to onreadystatechange event handler and check for readystate and response
             xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+
+                    // Parse the Json result
                     var result = JSON.parse(xmlhttp.response);
-                    if (result.success == "true") {
+
+                    // Check if operation was successful and copy the new content to be the originial
+                    // content so we don't have to refresh the page.
+                    if (result.success === 'true') {
                         contentElementMain.innerHTML = contentElement.value.replace(/\n/g, '<br>');
-                        subjectElementMain.innerHTML = subjectElement.value
+                        subjectElementMain.innerHTML = subjectElement.value;
                         var post = document.getElementsByClassName('post')[0];
-                        post.style.display = "block";
+                        post.style.display = 'block';
 
                         var editForm = document.getElementsByClassName('edit-post')[0];
-                        editForm.style.display = "none";
+                        editForm.style.display = 'none';
                     }
+
+                    // Show the error if any error is in the response from server
                     else if (result.error) {
                         errorElement.innerHTML = result.error;
                     }
@@ -60,136 +83,179 @@
                         errorElement.innerHTML = 'Unknown Error';
                     }
                 }
-            }
+            };
 
+            // Variable to hold the values to be sent in the edit-post request
             var subject = subjectElement.value;
             var content = contentElement.value;
-            var post_id = postIdElement.value;
+            var postId = postIdElement.value;
 
-            xmlhttp.open("POST", window.location.href, true);
+            // Set the headers, method and other stuff for the request and send it with the
+            // parameters required by the server
+            xmlhttp.open('POST', window.location.href, true);
             xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xmlhttp.send('action=edit&subject=' + subject + '&content=' + content + '&post_id=' + post_id);
+            xmlhttp.send('action=edit&subject=' + subject + '&content=' + content + '&post_id=' + postId);
+
+            //return false so we don't submit the form as it is an ajax request!!
             return false;
-        }
+        };
     }
 
+    // Get all the elements which allow the user to edit the comment.
+    var editCommentElements = document.getElementsByClassName('edit-comment');
+
+    // Check if there are any elements on the page.
     if (editCommentElements) {
-        for (var i = 0; i < editCommentElements.length; i++) {
-            editCommentElements[i].addEventListener('click', function () {
 
-                var comment_id = this.getAttribute('data-comment');
-                var divElement = document.createElement('div');
-                divElement.setAttribute('class', 'col-xs-12');
-                var editElement = document.createElement('textarea');
-                editElement.setAttribute('class', 'form-control');
-                divElement.appendChild(editElement);
+        // function to be passed in to the click handler of each comment
+        var editCommentHandler = function () {
+
+            // Lots of dynamic elements to be generated so we can use them
+            // to allow users to edit their comments
+            var commentId = this.getAttribute('data-comment');
+            var divElement = document.createElement('div');
+            divElement.setAttribute('class', 'col-xs-12');
+            var editElement = document.createElement('textarea');
+            editElement.setAttribute('class', 'form-control');
+            divElement.appendChild(editElement);
 
 
-                var saveSpan = document.createElement('span');
-                var saveButton = document.createElement('button');
-                saveButton.setAttribute('type', 'button');
-                saveButton.innerHTML = "Save";
-                saveSpan.appendChild(saveButton);
+            var saveSpan = document.createElement('span');
+            var saveButton = document.createElement('button');
+            saveButton.setAttribute('type', 'button');
+            saveButton.innerHTML = 'Save';
+            saveSpan.appendChild(saveButton);
 
-                var cancelSpan = document.createElement('span');
-                var cancelButton = document.createElement('button');
-                cancelButton.setAttribute('type', 'button');
-                cancelButton.innerHTML = "Cancel";
-                cancelSpan.appendChild(cancelButton);
+            var cancelSpan = document.createElement('span');
+            var cancelButton = document.createElement('button');
+            cancelButton.setAttribute('type', 'button');
+            cancelButton.innerHTML = 'Cancel';
+            cancelSpan.appendChild(cancelButton);
 
-                var errorSpan = document.createElement('span');
-                errorSpan.setAttribute('class', 'text-danger');
+            var errorSpan = document.createElement('span');
+            errorSpan.setAttribute('class', 'text-danger');
 
-                var elementToReplace = document.getElementById('comment' + comment_id);
-                editElement.value = elementToReplace.querySelector('p').innerHTML;
+            var elementToReplace = document.getElementById('comment' + commentId);
+            editElement.value = elementToReplace.querySelector('p').innerHTML;
 
-                editElement.parentNode.insertBefore(errorSpan, editElement.nextSibling);
-                editElement.parentNode.insertBefore(cancelSpan, editElement.nextSibling);
-                editElement.parentNode.insertBefore(saveSpan, editElement.nextSibling);
+            editElement.parentNode.insertBefore(errorSpan, editElement.nextSibling);
+            editElement.parentNode.insertBefore(cancelSpan, editElement.nextSibling);
+            editElement.parentNode.insertBefore(saveSpan, editElement.nextSibling);
 
-                elementToReplace.parentNode.replaceChild(divElement, elementToReplace);
+            elementToReplace.parentNode.replaceChild(divElement, elementToReplace);
 
-                cancelButton.addEventListener('click', function () {
-                    divElement.parentNode.replaceChild(elementToReplace, divElement);
-                });
+            // Attach click event handler to dynamically generated cancel button
+            cancelButton.addEventListener('click', function () {
+                divElement.parentNode.replaceChild(elementToReplace, divElement);
+            });
 
-                saveButton.addEventListener('click', function () {
-                    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-                    xmlhttp.onreadystatechange = function () {
-                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                            var result = JSON.parse(xmlhttp.response);
-                            if (result.success == "true") {
-                                elementToReplace.querySelector('p').innerHTML = editElement.value;
-                                divElement.parentNode.replaceChild(elementToReplace, divElement);
-                            }
-                            else if (result.comment_error) {
-                                errorSpan.innerHTML = result.comment_error;
-                            }
-                            else {
-                                errorSpan.innerHTML = 'Unknown Error';
-                            }
+            // Attach click event handler to dynamically generated save button
+            saveButton.addEventListener('click', function () {
+
+                // Create new ajax request with fallback to ActiveXobject
+                var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() :
+                new window.ActiveXObject('Microsoft.XMLHTTP');
+
+                // Attach to onreadystatechange event handler and check for readystate and response
+                xmlhttp.onreadystatechange = function () {
+                    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+
+                        // Parse the Json result
+                        var result = JSON.parse(xmlhttp.response);
+
+                        // Check if operation was successful and copy the new comment to be the originial
+                        // comment so we don't have to refresh the page.
+                        if (result.success === 'true') {
+                            elementToReplace.querySelector('p').innerHTML = editElement.value;
+                            divElement.parentNode.replaceChild(elementToReplace, divElement);
+                        }
+
+                        // Show the error if any error is in the response from server
+                        else if (result.comment_error) {
+                            errorSpan.innerHTML = result.comment_error;
+                        }
+                        else {
+                            errorSpan.innerHTML = 'Unknown Error';
                         }
                     }
+                };
 
-                    var post_id = postIdElement.value;
 
-                    xmlhttp.open("POST", '/comment', true);
-                    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                    xmlhttp.send('action=edit&comment=' + editElement.value + '&comment_id=' + comment_id + '&post_id=' + post_id);
-                    return false;
-                });
+                var postId = postIdElement.value;
 
-            })
+                // Set the headers, method and other stuff for the request and send it with the
+                // parameters required by the server
+                xmlhttp.open('POST', '/comment', true);
+                xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xmlhttp.send('action=edit&comment=' + editElement.value + '&comment_id=' + commentId +
+                             '&post_id=' + postId);
+                return false;
+            });
+        };
+
+        // Loop through all the editComment elements and attach to click event
+        for (var i = 0; i < editCommentElements.length; i++) {
+            editCommentElements[i].addEventListener('click', editCommentHandler);
 
         }
     }
 
-
+    // Get the like button element
     var likeButton = document.getElementById('like-post');
+
+    // Attach to click event if the button exists
     if (likeButton) {
         likeButton.addEventListener('click', function () {
+
+            // Get the current value of the button
             var status = likeButton.getAttribute('data-value');
             var action = '';
-            if (status == 'like') {
+
+            // Make the action be opposite of what the current value is so it can act
+            // as a toggle
+            if (status === 'like') {
                 action = 'unlike';
             }
-            else if (status == 'unlike') {
+            else if (status === 'unlike') {
                 action = 'like';
             }
             else {
                 return;
             }
-            var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+            // Send an Ajax request and manipulate the button to be reversed upon each request.
+            // so it can act as a toggle button for changing the like/unlike on server
+            var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
             xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
                     var result = JSON.parse(xmlhttp.response);
-                    if (result.success == "true") {
-                        if (result.like_status == 'like') {
+                    if (result.success === 'true') {
+                        if (result.like_status === 'like') {
                             likeButton.setAttribute('class', 'btn btn-danger');
                             likeButton.setAttribute('data-value', 'like');
                             likeButton.innerHTML = '<i class=\"fa fa-thumbs-down\"></i> Unlike';
                         }
-                        else if (result.like_status == 'unlike') {
+                        else if (result.like_status === 'unlike') {
                             likeButton.setAttribute('class', 'btn btn-success');
                             likeButton.setAttribute('data-value', 'unlike');
                             likeButton.innerHTML = '<i class=\"fa fa-thumbs-up\"></i> Like';
                         }
                     }
                     else if (result.error) {
-                        alert(result.error);
+                        window.alert(result.error);
                     }
                     else {
-                        alert("Unknown error");
+                        window.alert('Unknown error');
                     }
                 }
-            }
+            };
 
-            var post_id = postIdElement.value;
+            var postId = postIdElement.value;
 
-            xmlhttp.open("POST", '/likes', true);
+            xmlhttp.open('POST', '/likes', true);
             xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xmlhttp.send('&post_id=' + post_id + '&action=' + action);
-        })
+            xmlhttp.send('&post_id=' + postId + '&action=' + action);
+        });
     }
 
-})();
+}());
--- assets/js/validate_signup.js
+++ assets/js/validate_signup.js
@@ -1,85 +1,93 @@
+//IIFE so we don't pollute the global namespace
 (function () {
-    var form = document.getElementsByTagName('form')[0];
-    var usernameElement = document.getElementsByName("username")[0];
-    var passwordElement = document.getElementsByName("password")[0];
-    var verifyElement = document.getElementsByName("verify")[0];
-    var emailElement = document.getElementsByName("email")[0];
-    var errorSpan = document.getElementById("error");
+    'use strict';
 
+    // Check if user name is between 4-20 characters and doesn't contain
+    // any special character except - and _
+    var isValidUserName = function (username) {
+        if (username) {
+            var userReg = new RegExp('^[a-zA-Z0-9_-]{4,20}$');
+            if (userReg.exec(username)) {
+                return null;
+            } else {
+                return ('User name is not valid. Please make sure it is between 4-20 characters' +
+                        'and doesn\'t contain any special symbols.');
+            }
+        } else {
+            return ('*Username can\'t be empty.');
+        }
+    },
+
+        // Check if password is between 6-30 characters and matches with the verify password
+        isValidPassword = function (password, verify) {
+            if (password && verify) {
+                if (password !== verify) {
+                    return ('Password and confirmation password do not match');
+                } else {
+                    var passReg = new RegExp('^.{6,30}$');
+                    if (passReg.exec(password)) {
+                        return null;
+                    } else {
+                        return ('Password is not valid. Please make sure it is between 6-30 characters.');
+                    }
+                }
+            } else {
+                return ('Password or confirmation password can\'t be empty.');
+            }
+        },
+
+        // function to validate the email, using a simple regex because we don't want
+        // to block anyone with a "valid enough" email address
+        isValidEmail = function (email) {
+            if (email) {
+                var emailReg = new RegExp('^[\\S]+@[\\S]+.[\\S]+$');
+                if (emailReg.exec(email)) {
+                    return null;
+                } else {
+                    return ('Email address entered is not valid');
+                }
+
+            } else {
+                return null;
+            }
+        },
+
+    // Variables for elements associated with form validation
+        form = document.getElementsByTagName('form')[0],
+        usernameElement = document.getElementsByName('username')[0],
+        passwordElement = document.getElementsByName('password')[0],
+        verifyElement = document.getElementsByName('verify')[0],
+        emailElement = document.getElementsByName('email')[0],
+        errorSpan = document.getElementById('error');
+
+    //Attach on submit event handler to the form and validate data before submitting
     form.onsubmit = function () {
-        var username = usernameElement.value;
-        var password = passwordElement.value;
-        var verify = verifyElement.value;
-        var email = emailElement.value;
 
-        var userNameError = IsValidUserName(username);
+        // Variable to store value of the form validation elements
+        var username = usernameElement.value,
+            password = passwordElement.value,
+            verify = verifyElement.value,
+            email = emailElement.value,
+
+            userNameError = isValidUserName(username),
+            passwordError = isValidPassword(password, verify),
+            emailError = isValidEmail(email);
+
+        //Return false if any of the validation fails otherwise return true and submit the form
         if (userNameError) {
             errorSpan.innerHTML = userNameError;
             return false;
         }
 
-        var passwordError = IsValidPassword(password, verify);
         if (passwordError) {
             errorSpan.innerHTML = passwordError;
             return false;
         }
 
-        var emailError = IsValidEmail(email);
         if (emailError) {
             errorSpan.innerHTML = emailError;
             return false;
         }
         return true;
     };
-
-    var IsValidUserName = function (username) {
-        if (username) {
-            var userReg = new RegExp('^[a-zA-Z0-9_-]{4,20}$');
-            if (userReg.exec(username)) {
-                return null;
-            }
-            else {
-                return ('User name is not valid. Please make sure it is between 4-20 characters and doesn\'t contain any special symbols.');
-            }
-        }
-        else {
-            return ('*Username can\'t be empty.');
-        }
-    };
-
-    var IsValidPassword = function (password, verify) {
-        if (password && verify) {
-            if (password != verify) {
-                return ('Password and confirmation password do not match');
-            }
-            else {
-                var passReg = new RegExp('^.{6,30}$');
-                if (passReg.exec(password)) {
-                    return null;
-                }
-                else {
-                    return ('Password is not valid. Please make sure it is between 6-30 characters.');
-                }
-            }
-        }
-        else {
-            return ('Password or confirmation password can\'t be empty.');
-        }
-    };
-    
-    var IsValidEmail = function (email) {
-        if(email){
-            var emailReg = new RegExp('^[\\S]+@[\\S]+.[\\S]+$');
-            if(emailReg.exec(email)){
-                return null;
-            }
-            else{
-                return('Email address entered is not valid');
-            }
-
-        }
-        else{
-            return null;
-        }
-    };
-})();
+}());
