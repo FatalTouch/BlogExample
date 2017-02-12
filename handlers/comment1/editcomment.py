05e2d94bd8a58d5f4c816cc60f84336e84b90ff1
Mohit Mittal <contact@mohitmittal.me>
2017-02-12 22:25:57 +0530

    Documented and cleaned newcomment.py and created boilerplate for the new editcomment.py

--- handlers/comment1/editcomment.py
+++ handlers/comment1/editcomment.py
@@ -1,26 +1,30 @@
 from handlers import ViewHandler
 from utility import validate
 import json
+import time
 import entities
 
 
-# Handler for new comments
+# Handler for comments
 class NewCommentHandler(ViewHandler):
 
-    # This handler only accepts post requests and we make sure that post is
-    # valid and user is authenticated with the decorators and pass in the
-    # json parameter to the decorator to send json response in case of error
+    # This one only contains post handler!
     @ViewHandler.is_post_valid('json')
     @ViewHandler.is_user_authenticated('json')
     def post(self, post_id, post):
-        # Get the comment from the request
+
         comment = self.request.get("comment")
         params = {"comment": comment}
+        has_error = False
 
         # Check if the comment is valid
         comment_error = validate.is_valid_comment(comment)
         if comment_error:
             params["error"] = comment_error
+            has_error = True
+
+        # If there are any errors return the json response to the user
+        if has_error:
             self.response.write(json.dumps(params))
         else:
             # Create the comment object in the database
@@ -28,9 +32,11 @@ class NewCommentHandler(ViewHandler):
                        .create(comment, self.user.username,
                                post_id))
 
-            # if comment was created then we return the relevant data in
-            # json format to the user otherwise we return an error
             if comment:
+                # wait 0.1 seconds so we can properly fetch the
+                # newly created object from database
+                time.sleep(0.1)
+
                 params["success"] = "true"
                 params["username"] = comment.username
                 params["created"] = comment.created.strftime('%Y-%m-%d %H:%M:%S')
